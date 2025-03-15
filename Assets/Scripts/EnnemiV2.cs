427deeb66b9c2730c3187e078ae31ea090de3c21
Jomare59 <150144237+Jomare59@users.noreply.github.com>
2025-03-15 12:10:15 +0100

    Ajout du skin skeleton pour l'ennemi

--- Assets/Scripts/EnnemiV2.cs
+++ Assets/Scripts/EnnemiV2.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class EnnemiV2 : MonoBehaviour{
     private Transform cibleJoueur;
     private Vector3 ciblePosition;
+    [SerializeField] private Animator animator;
+    private bool dead = false;
     [SerializeField] private GameObject PrefabEffetMort;
     [SerializeField] private int life = 100;
     [SerializeField] private float vitesse = 1f;
@@ -13,15 +15,16 @@ public class EnnemiV2 : MonoBehaviour{
         RecupererJoueurPosition();
     }
     void Update(){
-        Vector3 direction = ciblePosition - transform.position;
-        if(direction.magnitude < 2f){
-            TouchJoueur();
+        if(!dead){
+            Vector3 direction = ciblePosition - transform.position;
+            if(direction.magnitude < 2f){
+                TouchJoueur();
+            }
+            direction = direction.normalized;
+            direction.y = 0; 
+            transform.Translate(Time.deltaTime * vitesse * direction /10, Space.World);
+            transform.LookAt(ciblePosition);
         }
-        direction = direction.normalized;
-        direction.y = 0; 
-        transform.Translate(Time.deltaTime * vitesse * direction /10, Space.World);
-        transform.LookAt(ciblePosition);
-
 
     }
     private void RecupererJoueurPosition(){
@@ -30,16 +33,20 @@ public class EnnemiV2 : MonoBehaviour{
     }
 
     public void TakeDamage(int nbDamage){
-        life -= nbDamage;
-        if(life <= 0){
-            PlayerStats.money += valueMoney;
-            Mort();
+        if(!dead){
+            life -= nbDamage;
+            if(life <= 0){
+                PlayerStats.money += valueMoney;
+                Mort();
+            }
         }
     }
 
     private void Mort(){
+        dead = true;
         EffetMort();
-        Destroy(gameObject);    
+        animator.SetBool("Dead", true);
+        Destroy(gameObject, 2f);    
     }
     private void EffetMort(){
         GameObject effet = Instantiate(PrefabEffetMort, transform.position, transform.rotation); //On instancie l'effet d'impact.
@@ -49,5 +56,9 @@ public class EnnemiV2 : MonoBehaviour{
     private void TouchJoueur(){
         Mort();
         GameManagerV2.instance.TakeDamageJoueur();
+    }
+
+    public bool GetBoolDead(){
+        return this.dead;
     }
 }
